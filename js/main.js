c8d3a6c7997eba0ad6af9ffc776330c54b9b3ea4
morfiy <keks46@yandex.ru>
2025-01-24 16:36:17 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $(".nav-link").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
+$(document).ready(function () {
+    $(".footer-menu a").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
